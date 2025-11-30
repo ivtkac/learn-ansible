6945b7d4e049c0d497045f43705c91f8066bab32
Ivan Tkachuk <ivtkac@gmail.com>
2025-11-30 23:28:05 +0200

    Add script to get private ips of webservers

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,25 +49,47 @@ resource "aws_instance" "ansible_control_server" {
   vpc_security_group_ids = [aws_security_group.ansible_sg.id]
 
   user_data = <<EOF
+    #!/bin/bash
+
+    # Install Ansible
     sudo apt-get update
     sudo apt-get upgrade -y
     sudo apt-get install software-properties-common
     sudo apt-add-repository ppa:ansible/ansible
     sudo apt-get install ansible -y
+
+    # Setup SSH key for ansible user
+    mkdir -p /home/ubuntu/.ssh
+    cat <<'SSHKEY' > /home/ubuntu/.ssh/id_rsa
+    ${tls_private_key.generated_key.private_key_pem}
+    SSHKEY
+
+    chmod 600 /home/ubuntu/.ssh/id_rsa
+    chown ubuntu:ubuntu /home/ubuntu/.ssh/id_rsa
+
+    # Create Ansible inventory
+    cat <<'INVENTORY' > /etc/ansible/hosts
+    [webservers]
+    ${join("\n", formatlist("%s  # %s", aws_instance.web_servers[*].private_ip, aws_instance.web_servers[*].tags.Name))}
+    INVENTORY
   EOF
 
   tags = {
     Name = "ansible-control"
   }
+
+  depends_on = [aws_instance.web_servers]
 }
 
+
 resource "aws_instance" "web_servers" {
-  count                  = length(var.web_servers)
-  ami                    = var.ami
-  instance_type          = var.instance
-  vpc_security_group_ids = [aws_security_group.ansible_sg.id]
+  count         = length(var.web_servers)
+  ami           = var.ami
+  instance_type = var.instance
+  key_name      = aws_key_pair.ansible_key_pair.key_name
 
   tags = {
     Name = element(var.web_servers, count.index)
   }
 }
+
