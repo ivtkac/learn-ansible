21ffe05fa466885987352ccaa57f7eeb251ebd45
Ivan Tkachuk <ivtkac@gmail.com>
2025-12-01 22:29:41 +0200

    Minor changes for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,18 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
     }
   }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,3 @@
-
 output "ansible_control_server_public_ip" {
   value       = aws_instance.ansible_control_server.public_ip
   description = "Public IP for Ansible control server"
