867e4d2f9b4c4b2edd39160a366062c7ffde989a
Ivan Tkachuk <ivtkac@gmail.com>
2025-12-01 11:02:11 +0200

    Some minor changes

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,20 +1,17 @@
 locals {
-  ansible_inventory = join("\n", formatlist("%s  # %s",
-    aws_instance.web_servers[*].private_ip,
-    aws_instance.web_servers[*].tags.Name
-  ))
+  key_name = aws_key_pair.ansible_key_pair.key_name
 }
 
 resource "aws_instance" "ansible_control_server" {
   ami                         = var.ami
   instance_type               = var.instance
-  key_name                    = aws_key_pair.ansible_key_pair.key_name
+  key_name                    = local.key_name
   vpc_security_group_ids      = [aws_security_group.ansible_sg.id]
   user_data_replace_on_change = true
 
-  user_data = templatefile("${path.module}/templates/user_data.sh.tftpl", {
+  user_data = templatefile("${path.module}/templates/ansible_install.sh.tftpl", {
     private_key = tls_private_key.generated_key.private_key_pem
-    inventory   = local.ansible_inventory
+    webservers  = aws_instance.web_servers[*]
   })
 
   tags = {
@@ -28,7 +25,7 @@ resource "aws_instance" "web_servers" {
   count                  = length(var.web_servers)
   ami                    = var.ami
   instance_type          = var.instance
-  key_name               = aws_key_pair.ansible_key_pair.key_name
+  key_name               = local.key_name
   vpc_security_group_ids = [aws_security_group.ansible_sg.id]
 
   tags = {
