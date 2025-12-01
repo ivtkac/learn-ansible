a6488c25d4558e7668895c5ad55a0b2265632e5a
Ivan Tkachuk <ivtkac@gmail.com>
2025-12-01 22:53:40 +0200

    Add backend terraform state to s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "s3" {
+    bucket         = "my-terraform-532334143223"
+    key            = "ansible-infrastructure/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
